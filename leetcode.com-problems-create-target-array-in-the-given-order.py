ecf25e2ca853a67a43de66f1c9f0149795c69415
fawaz <fawazfkp2@gmail.com>
2022-07-29 14:29:29 +0530

    14th problem added

---
+++ leetcode.com-problems-create-target-array-in-the-given-order.py
@@ -0,0 +1,14 @@
+nums=[0,1,2,3,4]
+index = [0,1,2,2,1]
+
+# method one
+# newnums = []
+# for i in range(len(nums)):
+#     newnums.insert(index[i],nums[i])
+# print(newnums)
+
+# method two
+array= []
+for i, j in zip(nums,index):
+    array.insert(j,i)
+print(array)
