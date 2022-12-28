8cbee4fbdd3c3f7504931031c06a8f90c9ffd224
fawaz <fawaz.k@eastvantage-europe.com>
2022-12-28 15:09:28 +0530

    add another folder named machine test

---
+++ leetcode-problems/Array/leetcode.com-problems-flipping-an-image.py
@@ -0,0 +1,20 @@
+
+
+from typing import List
+
+
+class Solution:
+    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+        for i in range(len(image)):
+            image[i].reverse()
+            for j in range(len(image[i])):
+                if image[i][j] == 1:
+                    image[i][j] = 0
+                elif image[i][j] == 0:
+                    image[i][j] = 1
+        return image
+
+
+s = Solution()
+image = [[1,1,0],[1,0,1],[0,0,0]]
+print(s.flipAndInvertImage(image))
