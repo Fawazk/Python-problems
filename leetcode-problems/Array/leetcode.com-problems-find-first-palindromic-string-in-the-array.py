21612407fd91de0173991f1d35385c1ab709c8c0
fawaz <fawaz.k@eastvantage-europe.com>
2023-01-12 17:07:50 +0530

    leetcode problem added

---
+++ leetcode-problems/Array/leetcode.com-problems-find-first-palindromic-string-in-the-array.py
@@ -0,0 +1,14 @@
+
+from typing import List
+
+
+class Solution:
+    def firstPalindrome(self, words: List[str]) -> str:
+        for data in words:
+            if data[::-1] == data:
+                return data
+        return ""
+
+s = Solution()
+words =["abc","car","ada","racecar","cool"]
+print(s.firstPalindrome(words))
